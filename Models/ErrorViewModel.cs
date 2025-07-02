45a46b3add9a636a09bf2605326496074bd4d228
Magd98 <magdmohammed998@gmail.com>
2025-07-02 11:12:24 +0300

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GameOnlineStore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
